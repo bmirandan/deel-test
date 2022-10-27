a370fad0c100216b0e21d9787c986fa1e9d16369
benja <benjamin@boldware.cl>
2022-10-27 11:10:42 -0300

    add best profession

--- src/app.js
+++ src/app.js
@@ -11,34 +11,47 @@ app.set('models', sequelize.models)
 /**
  * @returns contract by id
  */
+
 app.get('/contracts/:id', getProfile, async (req, res) => {
-    const { Contract } = req.app.get('models')
-    const { id } = req.params
-    const { id: idProfile } = req.profile
-    const contract = await Contract.findOne({
-        where:
-        {
-            id, [Op.or]: [{ ClientId: idProfile }, { ContractorId: idProfile }],
-        }
-    })
-    if (!contract) return res.status(404).end()
-    res.json(contract)
+    try {
+        const { Contract } = req.app.get('models')
+        const { id } = req.params
+        const { id: idProfile } = req.profile
+        const contract = await Contract.findOne({
+            where:
+            {
+                id, [Op.or]: [{ ClientId: idProfile }, { ContractorId: idProfile }],
+            }
+        })
+        if (!contract) return res.status(404).end()
+        res.json(contract)
+    } catch (error) {
+        console.log(error)
+        res.status(500).end()
+    }
 })
+
 /**
  * @returns contract that are not terminated
  */
+
 app.get('/contracts', getProfile, async (req, res) => {
-    const { Contract } = req.app.get('models')
-    const { id: idProfile } = req.profile
-    const contract = await Contract.findAll({
-        where:
-        {
-            [Op.or]: [{ ClientId: idProfile }, { ContractorId: idProfile }],
-            status: { [Op.not]: 'terminated' }
-        }
-    })
-    if (!contract) return res.status(404).end()
-    res.json(contract)
+    try {
+        const { Contract } = req.app.get('models')
+        const { id: idProfile } = req.profile
+        const contract = await Contract.findAll({
+            where:
+            {
+                [Op.or]: [{ ClientId: idProfile }, { ContractorId: idProfile }],
+                status: { [Op.not]: 'terminated' }
+            }
+        })
+        if (!contract) return res.status(404).end()
+        res.json(contract)
+    } catch (error) {
+        console.error(err.message) // probably a logger should be used
+        res.status(500).end()
+    }
 })
 
 /**
@@ -46,23 +59,29 @@ app.get('/contracts', getProfile, async (req, res) => {
  */
 
 app.get('/jobs/unpaid', getProfile, async (req, res) => {
-    const { Job, Contract } = req.app.get('models')
-    const { id: idProfile } = req.profile
-    const jobs = await Job.findAll({
-        where:
-        {
-            paid: { [Op.not]: true },
-        },
-        include: {
-            model: Contract,
-            where: {
-                [Op.or]: [{ ClientId: idProfile }, { ContractorId: idProfile }],
-                status: { [Op.not]: 'terminated' }
+    try {
+        const { Job, Contract } = req.app.get('models')
+        const { id: idProfile } = req.profile
+        const jobs = await Job.findAll({
+            where:
+            {
+                paid: { [Op.not]: true },
+            },
+            include: {
+                model: Contract,
+                where: {
+                    [Op.or]: [{ ClientId: idProfile }, { ContractorId: idProfile }],
+                    status: { [Op.not]: 'terminated' }
+                }
             }
-        }
-    })
-    if (!jobs) return res.status(404).end()
-    res.json(jobs)
+        })
+        if (!jobs) return res.status(404).end()
+
+        res.json(jobs)
+    }
+    catch (err) {
+        console.error(err.message) // probably a logger should be used
+    }
 })
 
 /**
@@ -90,10 +109,12 @@ app.post('/jobs/:job_id/pay', getProfile, async (req, res) => {
         res.json(job)
     }
     catch (err) {
+        console.error(err.message) // probably a logger should be used
         await transaction.rollback()
         return res.status(500).end()
     }
 })
+
 /**
  * @returns deposit to client balance 
  */
@@ -102,12 +123,12 @@ app.post('/balances/deposit/:userId', async (req, res) => {
     const { Job, Contract, Profile } = req.app.get('models')
     const { userId } = req.params
     const { amount } = req.body
-    if(!amount){return res.status(401).end()}
+    if (!amount) { return res.status(401).end() }
     const transaction = await sequelize.transaction();
     try {
         const profile = await Profile.findOne({ where: { id: userId, type: 'client' } })
         if (!profile) throw new Error('Profile not found!')
-        const jobs = await Job.findAll({
+        const jobsSum = await Job.findAll({
             where: {
                 paid: { [Op.not]: true },
             },
@@ -117,13 +138,15 @@ app.post('/balances/deposit/:userId', async (req, res) => {
                     ClientId: userId,
                     status: { [Op.not]: 'terminated' }
                 }
-            }
+            },
+            raw: true,
+            attributes: [[sequelize.fn('sum', sequelize.col('price')), 'sum']]
         })
-        const totalJobs = jobs.reduce((acc, job) => acc + job.price, 0)
+        const totalJobs = jobsSum[0].sum
         if (amount > totalJobs * 0.25) throw new Error('Amount is too high!')
         await profile.update({ balance: profile.balance + amount }, { transaction })
         await transaction.commit();
-        res.json(profile)
+        res.json({ message: "Balance updated!!" })
     }
     catch (err) {
         console.error(err.message) // probably a logger should be used
@@ -132,8 +155,45 @@ app.post('/balances/deposit/:userId', async (req, res) => {
     }
 })
 
+/**
+ * @returns admin/best-profession'
+ */
 
+app.get('/admin/best-profession', async (req, res) => {
+    try {
+        const { Job, Contract, Profile } = req.app.get('models')
+        const { start = '2020-08-03', end = '2020-08-15' } = req.query
 
+        const jobs = await Job.findAll({
+            raw: true,
+            attributes: ['Contract.Contractor.profession', [sequelize.fn('sum', sequelize.col('price')), 'sum']],
+            where: {
+                paid: true,
+                paymentDate: { [Op.between]: [start, end] }
+            },
+            include: {
+                model: Contract,
+                attributes: ['ContractorId'],
+                include: {
+                    model: Profile,
+                    attributes: ['profession'],
+                    where: { type: 'contractor' },
+                    as: 'Contractor'
+                },
+            },
+            group: ['Contract.Contractor.profession'],
+            order: [[sequelize.fn('sum', sequelize.col('price')), 'DESC']],
+            limit: 1
+        })
+        const bestProfession = jobs[0]?.profession
+        res.json({ bestProfession })
+    }
+    catch (err) {
+        console.error(err.message)
+        return res.status(500).end()
+    }
+})
 
 
 module.exports = app;
+
