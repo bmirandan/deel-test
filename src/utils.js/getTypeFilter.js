0095b644f6357e4b45d9be5909ece2ce21232414
benja <benjamin@boldware.cl>
2022-10-27 11:45:25 -0300

    removes unused function

--- src/utils.js/getTypeFilter.js
+++
@@ -1,9 +0,0 @@
-
-const getTypeFilter = async (req, res, next) => {
-    const { profile: { id, type } } = req
-    const typeFormated = `${type.charAt(0).toUpperCase()}${type.slice(1)}Id`
-    req.typeFilter = { [typeFormated]: id }
-    if (!type) return res.status(401).end()
-    next()
-}
-module.exports = { getTypeFilter }
